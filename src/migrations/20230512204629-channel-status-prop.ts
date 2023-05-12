0eb51add2aaa62a1063cb699550144e20c407763
Felipe Mantilla <developer@lulo.love>
2023-05-12 21:25:01 +0000

    await migration

--- src/migrations/20230512204629-channel-status-prop.ts
+++ src/migrations/20230512204629-channel-status-prop.ts
@@ -3,7 +3,7 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up(queryInterface, Sequelize) {
-    queryInterface.addColumn(
+    await queryInterface.addColumn(
       "channels",
       "channel_status",
       {
